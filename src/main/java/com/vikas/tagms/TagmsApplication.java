5538ae0ee737d4c4ab31e799b44d2c5c0a01c9be
vikas <vikaskmr2411@gmail.com>
2025-09-08 22:33:46 +0530

    Initial Files

---
+++ src/main/java/com/vikas/tagms/TagmsApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.tagms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagmsApplication.class, args);
+	}
+
+}
